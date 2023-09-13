5f0851304bf80067fa61b8fe8d39fe1c45ab8b99
MiguelDN <mpsdiaz@gmail.com>
2023-09-13 18:06:10 +0200

    modificaciones

--- quickstart-react/src/App.js
+++ quickstart-react/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { useState, useEffect } from "react";
+// import { useState, useEffect } from "react";
 import "./App.css";
-import mondaySdk from "monday-sdk-js";
+// import mondaySdk from "monday-sdk-js";
 import "monday-ui-react-core/dist/main.css";
 //Explore more Monday React Components here: https://style.monday.com/
-import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
+// import AttentionBox from "monday-ui-react-core/dist/AttentionBox.js";
 import Objective from "./components/Objective";
 import Box from '@mui/material/Box';
-import ItemsObjetive from "./components/ItemsObjetive";
+// import ItemsObjetive from "./components/ItemsObjetive";
 
 // Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
-const monday = mondaySdk();
+// const monday = mondaySdk();
 
 /**
  * Represents the main application component.
@@ -22,24 +22,24 @@ const monday = mondaySdk();
  * @returns {JSX.Element} The rendered React component.
  */
 const App = () => {
-  const [context, setContext] = useState();
+  // const [context, setContext] = useState();
 
-  useEffect(() => {
-    // Notice this method notifies the monday platform that user gains a first value in an app.
-    // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
-    monday.execute("valueCreatedForUser");
+  // useEffect(() => {
+  //   // Notice this method notifies the monday platform that user gains a first value in an app.
+  //   // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#value-created-for-user/
+  //   monday.execute("valueCreatedForUser");
 
-    // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
-    monday.listen("context", (res) => {
-      setContext(res.data);
-    });
-  }, []);
+  //   // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
+  //   monday.listen("context", (res) => {
+  //     setContext(res.data);
+  //   });
+  // }, []);
 
   //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
-  const attentionBoxText = `Hello, your user_id is: ${
-    context ? context.user.id : "still loading"
-  }.
-  Let's start building your amazing app, which will change the world! HOLAAAAAA`;
+  // const attentionBoxText = `Hello, your user_id is: ${
+  //   context ? context.user.id : "still loading"
+  // }.
+  // Let's start building your amazing app, which will change the world! HOLAAAAAA`;
 
   return (
     <>
